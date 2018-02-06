e090e820942a4a487d103076a9a1d96861fbda9d
Louis Ritchie <louiscritchie@gmail.com>
2018-02-05 23:08:14 -0800

    add some basic info to the Done page, improve styles

--- components/pages/Done.jsx
+++ components/pages/Done.jsx
@@ -19,15 +19,26 @@ export default class Competition extends Component {
           <Navbar />
         </div>
         <div className="col-12 flex flex-center">
-          <div className="columns col-6">
-            <div className="col-12 text-center">
-              <h2>Thanks for signing up!</h2>
-            </div>
-            <div className="col-12">
+          <div className="columns col-6 col-sm-10">
+            <section className="col-12 text-center">
+              <h2>You're signed up for HackUVic!</h2>
+            </section>
+
+            <section className="columns col-12 flex flex-center">
+              <h3 className="text-center">Here's what you need to know.</h3>
+              <ul className="col-sm-12 col-6">
+                <li>Registration opens at 9am on Saturday, March 10th, on the 1st floor of ECS.</li>
+                <li>Opening ceremonies are being held at 10am in ECS 125 - Make sure you're there!</li>
+                <li>We'll take care of lunch and dinner on Saturday, and we'll serve breakfast, lunch, and dinner on Sunday.</li>
+                <li>Coffee and other refreshments will be provided throughout the event.</li>
+              </ul>
+            </section>
+
+            <section className="col-12">
               <div className="btn btn-primary btn-block btn-fat">
                 <Link to="/">Back to HackUVic Homepage</Link>
               </div>
-            </div>
+            </section>
           </div>
         </div>
       </main>
