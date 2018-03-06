145cc4dd2a26f75af3dbf1ff13c23381a0846005
Dominique Charlebois <domo.charlebois@gmail.com>
2018-03-06 10:19:07 -0800

    Changed Team size to 4 to reflect what we have on Devpost

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -21,7 +21,7 @@ export default class About extends Component {
             </div>
             <div className="column col-sm-12 col-6">
               <p>HackUVic is a hackathon hosted by the Uvic Web Design and Development Club at the University of Victoria. It's an event for developers and designers of all skill levels to build their own project from scratch.</p>
-              <p>The event is free and food will be provided. Prizes for winners. Recommended team size is 2 - 5, we'll pair you up other students attending the event if you can't find a team.</p>
+              <p>The event is free and food will be provided. Prizes for winners. Recommended team size is 2 - 4, we'll pair you up other students attending the event if you can't find a team.</p>
             </div>
           </div>
         </div>
